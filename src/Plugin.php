b4c51be1de670ffcb4ea711fc52d392ca8791f5f
Joe Huss <detain@interserver.net>
2017-06-15 06:36:51 -0400

    Work on the addons activation / deactivation codre and moving them over to the updated method for registering

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,16 +13,31 @@ class Plugin {
 		$service = $event->getSubject();
 		function_requirements('class.Addon');
 		$addon = new \Addon();
-		$addon->set_module('vps')->set_text('Additional GB')->set_text_match('Additional (.*) GB')
-			->set_cost(VPS_HD_COST)->set_require_ip(false)->set_enable(function() {
-				$service_info = $service_order->get_service_info();
-				$settings = get_module_settings($service_order->get_module());
-				require_once 'include/licenses/license.functions.inc.php';
-				myadmin_log($service_order->get_module(), 'info', "Activating $space GB additional HD space for {$settings['TBLNAME']} {$service_info[$settings['PREFIX'].'_id']}", __LINE__, __FILE__);
-				$GLOBALS['tf']->history->add($service_order->get_module() . 'queue', $service_info[$settings['PREFIX'] . '_id'], 'update_hdsize', $space, $service_info[$settings['PREFIX'] . '_custid']);
-			})->set_disable(function() {
-			})->register();
-		$service->add_addon($addon);
+		$addon->set_module('vps')
+			->set_text('Additional GB')
+			->set_text_match('Additional (.*) GB')
+			->set_cost(VPS_HD_COST)
+			->set_require_ip(false)
+			->set_enable(['Detain\MyAdminVpsHd\Plugins', 'Enable'])
+			->set_disable(['Detain\MyAdminVpsHd\Plugins', 'Disable'])
+			->register();
+		$service_order->add_addon($addon);
+	}
+
+	public static function Enable($service_order) {
+		$service_info = $service_order->get_service_info();
+		$settings = get_module_settings($service_order->get_module());
+		require_once 'include/licenses/license.functions.inc.php';
+		myadmin_log($service_order->get_module(), 'info', "Activating $space GB additional HD space for {$settings['TBLNAME']} {$service_info[$settings['PREFIX'].'_id']}", __LINE__, __FILE__);
+		$GLOBALS['tf']->history->add($service_order->get_module() . 'queue', $service_info[$settings['PREFIX'] . '_id'], 'update_hdsize', $space, $service_info[$settings['PREFIX'] . '_custid']);
+	}
+
+	public static function Disable($service_order) {
+		$service_info = $service_order->get_service_info();
+		$settings = get_module_settings($service_order->get_module());
+		require_once 'include/licenses/license.functions.inc.php';
+		myadmin_log($service_order->get_module(), 'info', "Activating $space GB additional HD space for {$settings['TBLNAME']} {$service_info[$settings['PREFIX'].'_id']}", __LINE__, __FILE__);
+		$GLOBALS['tf']->history->add($service_order->get_module() . 'queue', $service_info[$settings['PREFIX'] . '_id'], 'update_hdsize', $space, $service_info[$settings['PREFIX'] . '_custid']);
 	}
 
 	public static function Settings(GenericEvent $event) {
