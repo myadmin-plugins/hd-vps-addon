e0cc69ce47f27e8c2d53c6857fd3303d6cf8edc1
Joe Huss <detain@interserver.net>
2022-07-28 17:05:03 -0400

    formatting update

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,107 +11,107 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class Plugin
 {
-	public static $name = 'HD Space VPS Addon';
-	public static $description = 'Allows selling of HD Space Upgrades to a VPS.';
-	public static $help = '';
-	public static $module = 'vps';
-	public static $type = 'addon';
+    public static $name = 'HD Space VPS Addon';
+    public static $description = 'Allows selling of HD Space Upgrades to a VPS.';
+    public static $help = '';
+    public static $module = 'vps';
+    public static $type = 'addon';
 
-	/**
-	 * Plugin constructor.
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Plugin constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function getHooks()
-	{
-		return [
-			'function.requirements' => [__CLASS__, 'getRequirements'],
-			self::$module.'.load_addons' => [__CLASS__, 'getAddon'],
-			self::$module.'.settings' => [__CLASS__, 'getSettings']
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function getHooks()
+    {
+        return [
+            'function.requirements' => [__CLASS__, 'getRequirements'],
+            self::$module.'.load_addons' => [__CLASS__, 'getAddon'],
+            self::$module.'.settings' => [__CLASS__, 'getSettings']
+        ];
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Plugins\Loader $this->loader
-		 */
-		$loader = $event->getSubject();
-		$loader->add_page_requirement('vps_hdspace', '/../vendor/detain/myadmin-hd-vps-addon/src/vps_hdspace.php');
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getRequirements(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
+        $loader->add_page_requirement('vps_hdspace', '/../vendor/detain/myadmin-hd-vps-addon/src/vps_hdspace.php');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getAddon(GenericEvent $event)
-	{
-		/**
-		 * @var \ServiceHandler $service
-		 */
-		$service = $event->getSubject();
-		function_requirements('class.AddonHandler');
-		$addon = new \AddonHandler();
-		$addon->setModule(self::$module)
-			->set_text('Additional GB')
-			->set_text_match('Additional (.*) GB')
-			->set_cost(VPS_HD_COST)
-			->set_require_ip(false)
-			->setEnable([__CLASS__, 'doEnable'])
-			->setDisable([__CLASS__, 'doDisable'])
-			->register();
-		$service->addAddon($addon);
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getAddon(GenericEvent $event)
+    {
+        /**
+         * @var \ServiceHandler $service
+         */
+        $service = $event->getSubject();
+        function_requirements('class.AddonHandler');
+        $addon = new \AddonHandler();
+        $addon->setModule(self::$module)
+            ->set_text('Additional GB')
+            ->set_text_match('Additional (.*) GB')
+            ->set_cost(VPS_HD_COST)
+            ->set_require_ip(false)
+            ->setEnable([__CLASS__, 'doEnable'])
+            ->setDisable([__CLASS__, 'doDisable'])
+            ->register();
+        $service->addAddon($addon);
+    }
 
-	/**
-	 * @param \ServiceHandler $serviceOrder
-	 * @param                $repeatInvoiceId
-	 * @param bool           $regexMatch
-	 */
-	public static function doEnable(\ServiceHandler $serviceOrder, $repeatInvoiceId, $regexMatch = false)
-	{
-		$serviceInfo = $serviceOrder->getServiceInfo();
-		$settings = get_module_settings(self::$module);
-		$space = $regexMatch;
-		myadmin_log(self::$module, 'info', self::$name." Activating {$space} GB additional HD space for {$settings['TBLNAME']} {$serviceInfo[$settings['PREFIX'].'_id']}", __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
-		$GLOBALS['tf']->history->add(self::$module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', $space, $serviceInfo[$settings['PREFIX'].'_custid']);
-	}
+    /**
+     * @param \ServiceHandler $serviceOrder
+     * @param                $repeatInvoiceId
+     * @param bool           $regexMatch
+     */
+    public static function doEnable(\ServiceHandler $serviceOrder, $repeatInvoiceId, $regexMatch = false)
+    {
+        $serviceInfo = $serviceOrder->getServiceInfo();
+        $settings = get_module_settings(self::$module);
+        $space = $regexMatch;
+        myadmin_log(self::$module, 'info', self::$name." Activating {$space} GB additional HD space for {$settings['TBLNAME']} {$serviceInfo[$settings['PREFIX'].'_id']}", __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
+        $GLOBALS['tf']->history->add(self::$module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', $space, $serviceInfo[$settings['PREFIX'].'_custid']);
+    }
 
-	/**
-	 * @param \ServiceHandler $serviceOrder
-	 * @param                $repeatInvoiceId
-	 * @param bool           $regexMatch
-	 */
-	public static function doDisable(\ServiceHandler $serviceOrder, $repeatInvoiceId, $regexMatch = false)
-	{
-		$serviceInfo = $serviceOrder->getServiceInfo();
-		$settings = get_module_settings(self::$module);
-		require_once __DIR__.'/../../../../include/licenses/license.functions.inc.php';
-		$space = $regexMatch;
-		myadmin_log(self::$module, 'info', self::$name." Deactivating $space GB additional HD space for {$settings['TBLNAME']} {$serviceInfo[$settings['PREFIX'].'_id']}", __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
-		$GLOBALS['tf']->history->add(self::$module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', $space, $serviceInfo[$settings['PREFIX'].'_custid']);
-		add_output('Additional '.$space.' GB HD Space Removed And Canceled');
-		$email = $settings['TBLNAME'].' ID: '.$serviceInfo[$settings['PREFIX'].'_id'].'<br>'.$settings['TBLNAME'].' Hostname: '.$serviceInfo[$settings['PREFIX'].'_hostname'].'<br>Repeat Invoice: '.$repeatInvoiceId.'<br>Additional Space: '.$space.' GB<br>Description: '.self::$name.'<br>';
-		$subject = $settings['TBLNAME'].' '.$serviceInfo[$settings['PREFIX'].'_id'].' Canceled Additional '.$space.' GB HD Space';
-		(new \MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/vps_hdspace_canceled.tpl');
-	}
+    /**
+     * @param \ServiceHandler $serviceOrder
+     * @param                $repeatInvoiceId
+     * @param bool           $regexMatch
+     */
+    public static function doDisable(\ServiceHandler $serviceOrder, $repeatInvoiceId, $regexMatch = false)
+    {
+        $serviceInfo = $serviceOrder->getServiceInfo();
+        $settings = get_module_settings(self::$module);
+        require_once __DIR__.'/../../../../include/licenses/license.functions.inc.php';
+        $space = $regexMatch;
+        myadmin_log(self::$module, 'info', self::$name." Deactivating $space GB additional HD space for {$settings['TBLNAME']} {$serviceInfo[$settings['PREFIX'].'_id']}", __LINE__, __FILE__, self::$module, $serviceInfo[$settings['PREFIX'].'_id']);
+        $GLOBALS['tf']->history->add(self::$module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', $space, $serviceInfo[$settings['PREFIX'].'_custid']);
+        add_output('Additional '.$space.' GB HD Space Removed And Canceled');
+        $email = $settings['TBLNAME'].' ID: '.$serviceInfo[$settings['PREFIX'].'_id'].'<br>'.$settings['TBLNAME'].' Hostname: '.$serviceInfo[$settings['PREFIX'].'_hostname'].'<br>Repeat Invoice: '.$repeatInvoiceId.'<br>Additional Space: '.$space.' GB<br>Description: '.self::$name.'<br>';
+        $subject = $settings['TBLNAME'].' '.$serviceInfo[$settings['PREFIX'].'_id'].' Canceled Additional '.$space.' GB HD Space';
+        (new \MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/vps_hdspace_canceled.tpl');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Settings $settings
-		 **/
-		$settings = $event->getSubject();
-		$settings->add_text_setting(self::$module, _('Addon Costs'), 'vps_hd_cost', _('VPS Additional HD Space Cost'), _('This is the cost for purchasing additional HD space for a VPS.'), $settings->get_setting('VPS_HD_COST'));
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+        $settings->add_text_setting(self::$module, _('Addon Costs'), 'vps_hd_cost', _('VPS Additional HD Space Cost'), _('This is the cost for purchasing additional HD space for a VPS.'), $settings->get_setting('VPS_HD_COST'));
+    }
 }
--- src/vps_hdspace.php
+++ src/vps_hdspace.php
@@ -16,91 +16,91 @@
  */
 function vps_hdspace()
 {
-	$module = 'vps';
-	$settings = get_module_settings($module);
-	page_title('Purchase Additional VPS HD Space');
-	$db = get_module_db($module);
-	$db2 = clone $db;
-	$settings = get_module_settings($module);
-	$id = (int)$GLOBALS['tf']->variables->request['id'];
-	$serviceInfo = get_service($id, $module);
-	if ($serviceInfo === false) {
-		dialog('Invalid VPS', 'Invalid VPS ID Passed');
-		return false;
-	}
-	$extra = parse_vps_extra($serviceInfo['vps_extra']);
-	$table = new TFTable;
-	if (mb_strpos($_SERVER['PHP_SELF'], 'iframe.php') !== false) {
-		$table->set_post_location('iframe.php');
-	}
-	$table->add_hidden('id', $serviceInfo[$settings['PREFIX'].'_id']);
-	$table->set_title('Additional VPS Drive Space');
-	$table->add_field('VPS ID', 'r');
-	$table->add_field($serviceInfo['vps_id'], 'r');
-	$table->add_row();
-	$table->add_field('Hostname', 'r');
-	$table->add_field($serviceInfo['vps_hostname'], 'r');
-	$table->add_row();
-	if (isset($extra['diskused'])) {
-		$table->add_field('Current HD Used', 'r');
-		$table->add_field(round($extra['diskused'] / 1000000).' GB', 'r');
-		$table->add_row();
-		$table->add_field('Current HD Total', 'r');
-		$table->add_field(round($extra['diskmax'] / 1000000).' GB', 'r');
-		$table->add_row();
-	}
-	$costInfo = get_service_cost($serviceInfo, $module);
-	$frequency = $costInfo['frequency'];
-	$gbcost = round(get_reseller_price($module, 'hd', VPS_HD_COST), 2);
-	$cost = $gbcost;
-	$size = 0;
-	$repeat_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
-	$repeat_invoices = $repeat_invoice->find([['description','like','Additional % GB Space for VPS '.$serviceInfo['vps_id']]]);
-	if (count($repeat_invoices) > 0) {
-		$repeat_invoice->load_real($repeat_invoices[0]);
-		$rinv = $repeat_invoice->get_raw_row();
-		if (preg_match('/Additional (.*) GB Space/', $repeat_invoice->get_description(), $matches)) {
-			$size = $matches[1];
-		} else {
-			add_output('Unable to get current addon disk usage.. please contact support@interserver.net about this');
-			return false;
-		}
-		$table->add_field('Additional Space Ordered', 'r');
-		$table->add_field($size.' GB', 'r');
-		$table->add_row();
-	}
-	$cursize = $size;
-	if (isset($GLOBALS['tf']->variables->request['size'])) {
-		$size = (int)$GLOBALS['tf']->variables->request['size'];
-	}
-	$cost = $size * $cost;
-	$service_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
-	$service_invoice = $service_invoice->load_real($serviceInfo[$settings['PREFIX'].'_invoice']);
-	if ($service_invoice->loaded === true) {
-		$service_date = $db->fromTimestamp($service_invoice->get_date());
-		$new_date = date('Y-m').'-'.date('d', $service_date).' 01:01:01';
-		$curday = date('d');
-		$oday = date('d', $service_date);
-		if ($curday >= $oday) {
-			$diffday = $curday - $oday;
-		} else {
-			$diffday = $oday - $curday;
-		}
-		$daysinmonth = date('t');
-		$daycost = $cost / $daysinmonth;
-		$diffcost = number_format(($daysinmonth - $diffday) * $daycost, 2);
-	} else {
-		$new_date = date('Y-m-d 01:01:01');
-		$diffcost = $cost;
-	}
-	if ($frequency > 1) {
-		$diffcost = round($diffcost + ($cost * ($frequency - 1)), 2);
-	}
-	$cost = round($cost * $frequency, 2);
+    $module = 'vps';
+    $settings = get_module_settings($module);
+    page_title('Purchase Additional VPS HD Space');
+    $db = get_module_db($module);
+    $db2 = clone $db;
+    $settings = get_module_settings($module);
+    $id = (int)$GLOBALS['tf']->variables->request['id'];
+    $serviceInfo = get_service($id, $module);
+    if ($serviceInfo === false) {
+        dialog('Invalid VPS', 'Invalid VPS ID Passed');
+        return false;
+    }
+    $extra = parse_vps_extra($serviceInfo['vps_extra']);
+    $table = new TFTable();
+    if (mb_strpos($_SERVER['PHP_SELF'], 'iframe.php') !== false) {
+        $table->set_post_location('iframe.php');
+    }
+    $table->add_hidden('id', $serviceInfo[$settings['PREFIX'].'_id']);
+    $table->set_title('Additional VPS Drive Space');
+    $table->add_field('VPS ID', 'r');
+    $table->add_field($serviceInfo['vps_id'], 'r');
+    $table->add_row();
+    $table->add_field('Hostname', 'r');
+    $table->add_field($serviceInfo['vps_hostname'], 'r');
+    $table->add_row();
+    if (isset($extra['diskused'])) {
+        $table->add_field('Current HD Used', 'r');
+        $table->add_field(round($extra['diskused'] / 1000000).' GB', 'r');
+        $table->add_row();
+        $table->add_field('Current HD Total', 'r');
+        $table->add_field(round($extra['diskmax'] / 1000000).' GB', 'r');
+        $table->add_row();
+    }
+    $costInfo = get_service_cost($serviceInfo, $module);
+    $frequency = $costInfo['frequency'];
+    $gbcost = round(get_reseller_price($module, 'hd', VPS_HD_COST), 2);
+    $cost = $gbcost;
+    $size = 0;
+    $repeat_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
+    $repeat_invoices = $repeat_invoice->find([['description','like','Additional % GB Space for VPS '.$serviceInfo['vps_id']]]);
+    if (count($repeat_invoices) > 0) {
+        $repeat_invoice->load_real($repeat_invoices[0]);
+        $rinv = $repeat_invoice->get_raw_row();
+        if (preg_match('/Additional (.*) GB Space/', $repeat_invoice->get_description(), $matches)) {
+            $size = $matches[1];
+        } else {
+            add_output('Unable to get current addon disk usage.. please contact support@interserver.net about this');
+            return false;
+        }
+        $table->add_field('Additional Space Ordered', 'r');
+        $table->add_field($size.' GB', 'r');
+        $table->add_row();
+    }
+    $cursize = $size;
+    if (isset($GLOBALS['tf']->variables->request['size'])) {
+        $size = (int)$GLOBALS['tf']->variables->request['size'];
+    }
+    $cost = $size * $cost;
+    $service_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
+    $service_invoice = $service_invoice->load_real($serviceInfo[$settings['PREFIX'].'_invoice']);
+    if ($service_invoice->loaded === true) {
+        $service_date = $db->fromTimestamp($service_invoice->get_date());
+        $new_date = date('Y-m').'-'.date('d', $service_date).' 01:01:01';
+        $curday = date('d');
+        $oday = date('d', $service_date);
+        if ($curday >= $oday) {
+            $diffday = $curday - $oday;
+        } else {
+            $diffday = $oday - $curday;
+        }
+        $daysinmonth = date('t');
+        $daycost = $cost / $daysinmonth;
+        $diffcost = number_format(($daysinmonth - $diffday) * $daycost, 2);
+    } else {
+        $new_date = date('Y-m-d 01:01:01');
+        $diffcost = $cost;
+    }
+    if ($frequency > 1) {
+        $diffcost = round($diffcost + ($cost * ($frequency - 1)), 2);
+    }
+    $cost = round($cost * $frequency, 2);
 
-	if (!isset($GLOBALS['tf']->variables->request['confirm']) || $GLOBALS['tf']->variables->request['confirm'] != 'yes') {
-		$table->csrf('additional_hd');
-		$GLOBALS['tf']->add_html_head_js_string('
+    if (!isset($GLOBALS['tf']->variables->request['confirm']) || $GLOBALS['tf']->variables->request['confirm'] != 'yes') {
+        $table->csrf('additional_hd');
+        $GLOBALS['tf']->add_html_head_js_string('
 	jQuery(function() {
 		jQuery( "#hdslider" ).slider({
 			range: "min",
@@ -117,94 +117,94 @@ function vps_hdspace()
 		jQuery( "#size" ).val( jQuery( "#hdslider" ).slider( "value" ) );
 	});
 ');
-		$table->add_field('Cost Per Month', 'r');
-		$table->add_field('<input type=text id="hdamount" readonly=true style="border: none; text-align: right;" size=5>', 'r');
-		$table->add_row();
-		$table->add_field('Additional GB', 'r');
-		$table->add_field('<input type=text id="size" name="size" style="text-align: right;" size=5>', 'r');
-		$table->add_row();
-		$table->set_colspan(2);
-		$table->add_field('How Much Additional Space? (1-100 GB)');
-		$table->add_row();
-		$table->set_colspan(2);
-		$table->add_field('<div id="hdslider"></div>');
-		$table->add_row();
-		$table->set_colspan(2);
-		$table->add_field('Up to 100GB Additional HD Space can be purchased for $1.00 per 10 GB per Month');
-		$table->add_row();
-		$table->add_field('Confirm Purchase', 'r');
-		$table->add_field('<select name=confirm><option value=no>No</option><option value=yes>Yes</option></select>', 'r');
-		$table->add_row();
-		$table->set_colspan(2);
-		$table->add_field($table->make_submit('Continue'));
-		$table->add_row();
-		add_output($table->get_table());
-	} elseif (verify_csrf('additional_hd')) {
-		if ($size >= 1 && $size <= 100) {
-			myadmin_log('vps', 'info', 'Update Additional Drive Space Function Called', __LINE__, __FILE__);
-			myadmin_log('vps', 'info', '  VPS ID: '.$serviceInfo[$settings['PREFIX'].'_id'], __LINE__, __FILE__);
-			myadmin_log('vps', 'info', '  VPS Cust ID: '.$serviceInfo[$settings['PREFIX'].'_custid'], __LINE__, __FILE__);
-			myadmin_log('vps', 'info', '  VPS Hostname: '.$serviceInfo[$settings['PREFIX'].'_hostname'], __LINE__, __FILE__);
-			myadmin_log('vps', 'info', "	New Size: {$size}", __LINE__, __FILE__);
-			myadmin_log('vps', 'info', "	Previous Update Size: {$cursize}", __LINE__, __FILE__);
-			myadmin_log('vps', 'info', "	New Cost: {$cost}", __LINE__, __FILE__);
-			myadmin_log('vps', 'info', "	Diff Cost: {$diffcost}", __LINE__, __FILE__);
-			if ($size != $cursize) {
-				$description = 'Additional '.$size.' GB Space for '.$settings['TBLNAME'].' '.$serviceInfo[$settings['PREFIX'].'_id'];
-				// check if they previously purchased additional drive space
-				if ($cursize > 0) {
-					$rid = $repeat_invoice->get_id();
-					myadmin_log('vps', 'info', "	Updated Repeat Invoice $rid", __LINE__, __FILE__);
-					$repeat_invoice->setDescription($description)
-						->setCost($cost)
-						->save();
-					$invoice = new \MyAdmin\Orm\Invoice($db);
-					$invoices = $invoice->find([['type','=','1'],['extra','=',$rid],['date','>=',mysql_date_sub(mysql_now(), 'INTERVAL 1 MONTH')]]);
-					foreach ($invoices as $invoice_id) {
-						$invoice->load_real($invoice_id);
-						if ($invoice->get_paid() == 1) {
-							$diffcost = bcsub($diffcost, $invoice->get_amount(), 2);
-							myadmin_log('vps', 'info', '	Crediting '.$invoice->get_amount().' Due To Paid Invoice '.$invoice->get_id(), __LINE__, __FILE__);
-						} else {
-							sql_delete_by_id('invoices', $db->Record['invoices_id'], $serviceInfo[$settings['PREFIX'].'_custid'], $module);
-							myadmin_log('vps', 'info', '	Deleting Unpaid Invoice '.$db->Record['invoices_id'], __LINE__, __FILE__);
-						}
-					}
-				} else {
-					$repeat_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
-					$repeat_invoice->setId(null)
-						->setDescription($description)
-						->setType(1)
-						->setCost($cost)
-						->setCustid($serviceInfo[$settings['PREFIX'].'_custid'])
-						->setFrequency($frequency)
-						->setDate($new_date)
-						->setModule($module)
-						->setService($id)
-						->save();
-					$rid = $repeat_invoice->get_id();
-					myadmin_log('vps', 'info', "	Created new \\MyAdmin\\Orm\\Invoice {$rid}", __LINE__, __FILE__);
-				}
-				if ($diffcost > 0) {
-					$invoice = $repeat_invoice->invoice($new_date, (float)$diffcost, false);
-					$iid = $invoice->get_id();
-					myadmin_log('vps', 'info', "	Created Invoice {$iid} For {$diffcost}", __LINE__, __FILE__);
-					add_output('Invoice Created, Please Pay This To Activate Extra Space<br>');
-				} else {
-					myadmin_log('vps', 'info', '	Queued Drive Update', __LINE__, __FILE__, $module);
-					//got here if the space shrank
-					add_output('Repeat Invoice Updated, Server Size Update Queued');
-					$GLOBALS['tf']->history->add($module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', '', $serviceInfo[$settings['PREFIX'].'_custid']);
-				}
-				if (mb_strpos($_SERVER['PHP_SELF'], 'iframe.php') === false) {
-					function_requirements('view_vps');
-					view_vps($serviceInfo[$settings['PREFIX'].'_id']);
-				}
-			} else {
-				add_output('No Change Made, Size The Same');
-			}
-		} else {
-			add_output('Invalid Size Specified');
-		}
-	}
+        $table->add_field('Cost Per Month', 'r');
+        $table->add_field('<input type=text id="hdamount" readonly=true style="border: none; text-align: right;" size=5>', 'r');
+        $table->add_row();
+        $table->add_field('Additional GB', 'r');
+        $table->add_field('<input type=text id="size" name="size" style="text-align: right;" size=5>', 'r');
+        $table->add_row();
+        $table->set_colspan(2);
+        $table->add_field('How Much Additional Space? (1-100 GB)');
+        $table->add_row();
+        $table->set_colspan(2);
+        $table->add_field('<div id="hdslider"></div>');
+        $table->add_row();
+        $table->set_colspan(2);
+        $table->add_field('Up to 100GB Additional HD Space can be purchased for $1.00 per 10 GB per Month');
+        $table->add_row();
+        $table->add_field('Confirm Purchase', 'r');
+        $table->add_field('<select name=confirm><option value=no>No</option><option value=yes>Yes</option></select>', 'r');
+        $table->add_row();
+        $table->set_colspan(2);
+        $table->add_field($table->make_submit('Continue'));
+        $table->add_row();
+        add_output($table->get_table());
+    } elseif (verify_csrf('additional_hd')) {
+        if ($size >= 1 && $size <= 100) {
+            myadmin_log('vps', 'info', 'Update Additional Drive Space Function Called', __LINE__, __FILE__);
+            myadmin_log('vps', 'info', '  VPS ID: '.$serviceInfo[$settings['PREFIX'].'_id'], __LINE__, __FILE__);
+            myadmin_log('vps', 'info', '  VPS Cust ID: '.$serviceInfo[$settings['PREFIX'].'_custid'], __LINE__, __FILE__);
+            myadmin_log('vps', 'info', '  VPS Hostname: '.$serviceInfo[$settings['PREFIX'].'_hostname'], __LINE__, __FILE__);
+            myadmin_log('vps', 'info', "	New Size: {$size}", __LINE__, __FILE__);
+            myadmin_log('vps', 'info', "	Previous Update Size: {$cursize}", __LINE__, __FILE__);
+            myadmin_log('vps', 'info', "	New Cost: {$cost}", __LINE__, __FILE__);
+            myadmin_log('vps', 'info', "	Diff Cost: {$diffcost}", __LINE__, __FILE__);
+            if ($size != $cursize) {
+                $description = 'Additional '.$size.' GB Space for '.$settings['TBLNAME'].' '.$serviceInfo[$settings['PREFIX'].'_id'];
+                // check if they previously purchased additional drive space
+                if ($cursize > 0) {
+                    $rid = $repeat_invoice->get_id();
+                    myadmin_log('vps', 'info', "	Updated Repeat Invoice $rid", __LINE__, __FILE__);
+                    $repeat_invoice->setDescription($description)
+                        ->setCost($cost)
+                        ->save();
+                    $invoice = new \MyAdmin\Orm\Invoice($db);
+                    $invoices = $invoice->find([['type','=','1'],['extra','=',$rid],['date','>=',mysql_date_sub(mysql_now(), 'INTERVAL 1 MONTH')]]);
+                    foreach ($invoices as $invoice_id) {
+                        $invoice->load_real($invoice_id);
+                        if ($invoice->get_paid() == 1) {
+                            $diffcost = bcsub($diffcost, $invoice->get_amount(), 2);
+                            myadmin_log('vps', 'info', '	Crediting '.$invoice->get_amount().' Due To Paid Invoice '.$invoice->get_id(), __LINE__, __FILE__);
+                        } else {
+                            sql_delete_by_id('invoices', $db->Record['invoices_id'], $serviceInfo[$settings['PREFIX'].'_custid'], $module);
+                            myadmin_log('vps', 'info', '	Deleting Unpaid Invoice '.$db->Record['invoices_id'], __LINE__, __FILE__);
+                        }
+                    }
+                } else {
+                    $repeat_invoice = new \MyAdmin\Orm\Repeat_Invoice($db);
+                    $repeat_invoice->setId(null)
+                        ->setDescription($description)
+                        ->setType(1)
+                        ->setCost($cost)
+                        ->setCustid($serviceInfo[$settings['PREFIX'].'_custid'])
+                        ->setFrequency($frequency)
+                        ->setDate($new_date)
+                        ->setModule($module)
+                        ->setService($id)
+                        ->save();
+                    $rid = $repeat_invoice->get_id();
+                    myadmin_log('vps', 'info', "	Created new \\MyAdmin\\Orm\\Invoice {$rid}", __LINE__, __FILE__);
+                }
+                if ($diffcost > 0) {
+                    $invoice = $repeat_invoice->invoice($new_date, (float)$diffcost, false);
+                    $iid = $invoice->get_id();
+                    myadmin_log('vps', 'info', "	Created Invoice {$iid} For {$diffcost}", __LINE__, __FILE__);
+                    add_output('Invoice Created, Please Pay This To Activate Extra Space<br>');
+                } else {
+                    myadmin_log('vps', 'info', '	Queued Drive Update', __LINE__, __FILE__, $module);
+                    //got here if the space shrank
+                    add_output('Repeat Invoice Updated, Server Size Update Queued');
+                    $GLOBALS['tf']->history->add($module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'update_hdsize', '', $serviceInfo[$settings['PREFIX'].'_custid']);
+                }
+                if (mb_strpos($_SERVER['PHP_SELF'], 'iframe.php') === false) {
+                    function_requirements('view_vps');
+                    view_vps($serviceInfo[$settings['PREFIX'].'_id']);
+                }
+            } else {
+                add_output('No Change Made, Size The Same');
+            }
+        } else {
+            add_output('Invalid Size Specified');
+        }
+    }
 }
